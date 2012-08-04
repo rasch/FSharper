ceba9d356b875190b742273eda2faa76597d6105
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2012-08-05 00:53:02 +0400

    fixing lexer test before filing a bug

---
+++ src/fsharper/Tests/FSharpLexerTest.cs
@@ -0,0 +1,28 @@
+﻿namespace Tests
+{
+  using System.IO;
+  using JetBrains.ReSharper.Psi.FSharp.Parsing;
+  using JetBrains.ReSharper.Psi.Parsing;
+  using JetBrains.ReSharper.PsiTests.Lexing;
+  using JetBrains.Text;
+  using NUnit.Framework;
+
+  [TestFixture]
+  public class FSharpLexerTest : LexerTestBase
+  {
+    [Test] public void TestLexer()
+    {
+      DoTestFile(test1dot);
+    }
+
+    protected override ILexer CreateLexer(StreamReader sr)
+    {
+      return new FSharpLexer(new StringBuffer(sr.ReadToEnd()));
+    }
+
+    protected override string RelativeTestDataPath
+    {
+      get { return @"lexing\fsharp"; }
+    }
+  }
+}
