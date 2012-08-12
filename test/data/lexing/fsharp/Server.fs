216decca05d2f31a1caa616ad3035a9ac1c3b965
Denis Ok <denis.ok77@gmail.com>
2012-08-12 03:01:30 +0200

    Added test file
    * The **Server.fs** test file reveals that the '<|' operator (reverse
    pipeline) is not recognized by the lexer
    * There is a bug in the lexer visualizer: empty lines are not displayed,
    at least in my example (when trying to open **Server.fs**)

---
+++ test/data/lexing/fsharp/Server.fs
@@ -0,0 +1,49 @@
+﻿module BroadcastConsole
+
+open System
+open System.Collections.Generic
+
+/// Message type
+type Message = string
+
+/// Console history is a string sequence
+type ConsoleHistory = ResizeArray<Message>
+
+/// An in memory server that listens
+type Server() =
+    let channelHistoryMap = new Dictionary<_, ConsoleHistory>()
+    let channelListeners = new Dictionary<_, Event<Message>>()
+    let channelExists name = channelHistoryMap.ContainsKey(name)
+    let triggerUnknownChannelExn name =
+        failwith <| sprintf "Unknown channel name: %O" name
+
+    /// Register to a channel
+    member this.RegisterToChannel (channelName: string, handler: _) =
+        if channelExists channelName then
+            channelListeners.[channelName].Publish.Add(handler)
+        else
+            triggerUnknownChannelExn channelName
+
+    /// Returns the console history of a specific channel
+    member this.GetConsoleHistory (channelName: string) =
+        let ok, result = channelHistoryMap.TryGetValue(channelName)
+        if ok then  
+            result
+        else
+            triggerUnknownChannelExn channelName
+    
+    /// Initialize a console channel
+    member this.CreateChannel (channelName: string) =
+        if not (channelHistoryMap.ContainsKey(channelName)) then
+            channelHistoryMap.[channelName] <- new ConsoleHistory()
+            channelListeners.[channelName] <- new Event<_>()
+
+        true
+
+    /// Broadcast a message to a particular channel
+    member this.BroadcastMessage (channelName: string, msg: Message) =
+        if channelExists channelName then
+            channelHistoryMap.[channelName].Add(msg)
+            channelListeners.[channelName].Trigger(msg)
+        else
+            triggerUnknownChannelExn channelName
