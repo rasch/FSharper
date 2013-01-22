cc2d8ded21fd88e8f9296427bb1f52dccef88fbe
Matt Ellis <matt.ellis@jetbrains.com>
2013-01-22 21:10:02 +0000

    Added empty expression base classes

--- src/fsharper/Psi.FSharp/src/Impl/Tree/FSharpExpressionBase.cs
+++ src/fsharper/Psi.FSharp/src/Impl/Tree/FSharpExpressionBase.cs
@@ -1,11 +1,28 @@
 ﻿using JetBrains.ReSharper.Psi.ExtensionsAPI.Resolve.Managed;
+using JetBrains.ReSharper.Psi.FSharp.Tree;
+using JetBrains.ReSharper.Psi.FSharp.Tree.Impl;
 using JetBrains.ReSharper.Psi.Resolve;
 
-namespace JetBrains.ReSharper.Psi.FSharp.Impl.Tree
+namespace JetBrains.ReSharper.Psi.FSharp.Tree.Impl
 {
-  public class FSharpExpressionBase 
-    // : FSharpCompositeElement, IFSharpExpression, IManagedExpressionImpl, IAccessContext
+  abstract class FSharpExpressionBase : FSharpCompositeElement, IFSharpExpression//, IManagedExpressionImpl, IAccessContext
   {
-     
+  }
+
+  internal abstract class ReferenceExpressionBase : FSharpExpressionBase
+  {
+  }
+
+  internal abstract class OperatorExpressionBase : FSharpExpressionBase
+  {
+  }
+
+  internal abstract class TypeExpressionBase : FSharpExpressionBase
+  {
+  }
+
+  // TODO: Is this an expression?
+  internal abstract class PatternBase : FSharpExpressionBase
+  {
   }
 }
---
+++ src/fsharper/Psi.FSharp/src/Tree/IFSharpExpression.cs
@@ -0,0 +1,9 @@
+﻿using JetBrains.ReSharper.Psi.Resolve.Managed;
+
+namespace JetBrains.ReSharper.Psi.FSharp.Tree
+{
+    public interface IFSharpExpression// : IManagedExpression
+    {
+        // TODO: Think we need to create an interface in the .psi, like CSharpExpression 
+    }
+}
