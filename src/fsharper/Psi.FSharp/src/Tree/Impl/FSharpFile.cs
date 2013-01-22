056b6ab8f9f192cca9fb3d46674c555a7032a0fa
Matt Ellis <matt.ellis@jetbrains.com>
2013-01-22 20:37:31 +0000

    Fixing up FSharpFile generated code

---
+++ src/fsharper/Psi.FSharp/src/Tree/Impl/FSharpFile.cs
@@ -0,0 +1,10 @@
+﻿namespace JetBrains.ReSharper.Psi.FSharp.Tree.Impl
+{
+    internal partial class FSharpFile
+    {
+        public override PsiLanguageType Language
+        {
+            get { return FSharpLanguage.Instance; }
+        }
+    }
+}
