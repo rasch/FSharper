a4ae3d3354c2a654db559249f642d7880557ff7d
Matt Ellis <matt.ellis@jetbrains.com>
2013-01-23 14:45:22 +0000

    Allow running tests with plugins installed
    By default, ReSharper's test framework uses the normal ReSharper user
    settings folders when loading an environment to run tests, and this will
    load any plugins you normally have installed, which can cause tests to
    fail. This change will make it use a different folder, so plugins don't
    get loaded

--- src/fsharper/Tests/Properties/AssemblyInfo.cs
+++ src/fsharper/Tests/Properties/AssemblyInfo.cs
@@ -1,16 +1,43 @@
 using JetBrains.Application;
+using JetBrains.ReSharper;
 using JetBrains.ReSharper.Psi.FSharp.Parsing;
 using JetBrains.Threading;
 using System.Reflection;
 using System.Collections.Generic;
 using NUnit.Framework;
 
+
+[SetUpFixture]
+public class IsolatedReSharperTestEnvironmentAssembly : ReSharperTestEnvironmentAssembly
+{
+    public override IApplicationDescriptor CreateApplicationDescriptor()
+    {
+        return new IsolatedReSharperApplicationDescriptor();
+    }
+
+    private class IsolatedReSharperApplicationDescriptor : ReSharperApplicationDescriptor
+    {
+        public override string ProductName
+        {
+            get
+            {
+                // The product name is used to find settings under APPDATA. Leaving it at
+                // ReSharper would use your normal ReSharper settings (+plugins!) while
+                // running tests. This keeps them isolated
+                return base.ProductName + "_Isolated";
+            }
+        }
+    }
+}
+
+
+
 /// <summary>
 /// Test environment. Must be in the global namespace.
 /// </summary>
 [SetUpFixture]
 // ReSharper disable CheckNamespace
-public class TestEnvironmentAssembly : ReSharperTestEnvironmentAssembly
+public class TestEnvironmentAssembly : IsolatedReSharperTestEnvironmentAssembly
 // ReSharper restore CheckNamespace
 {
   /// <summary>
