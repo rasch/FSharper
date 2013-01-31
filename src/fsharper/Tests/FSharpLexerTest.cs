2dc8afff6f1c2620b9128534cd99786ab90330f1
Denis Ok <denis.ok77@gmail.com>
2013-01-31 02:51:51 +0100

    Adding tests for some comparison operators
    For those operators (test is not green)
    * ``<>``
    * ``<=``
    * ``>=``

--- src/fsharper/Tests/FSharpLexerTest.cs
+++ src/fsharper/Tests/FSharpLexerTest.cs
@@ -14,6 +14,7 @@
     // ReSharper disable StringLiteralTypo
     private readonly string[] files = new[]
     {
+      "comparison-operators-1",
       "do-bang",
       "int-literals",
       "let-arith-1",
