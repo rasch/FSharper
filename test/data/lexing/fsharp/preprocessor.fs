c25a5a6359017843823b18b63070ec3c9ddeabfe
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2013-02-04 15:50:36 +0400

    initial preprocessor support

---
+++ test/data/lexing/fsharp/preprocessor.fs
@@ -0,0 +1,15 @@
+    #light
+    #if VERSION1
+    #else 
+    #endif
+    #if INTERACTIVE
+    # 25
+    #line 25
+    #r "System.Core"
+    #I @"c:\Projects\Libraries\Bin"
+    #load "library.fs"
+    #time "on"
+    #help
+    #q
+    #quit
+    #endif
