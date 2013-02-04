435d84a538e74282b4e3e6c98797dcf878ac7578
Denis Ok <denis.ok77@gmail.com>
2013-02-04 02:19:23 +0100

    Fixed popup display bug in LexerVisualization
    Happens with operator keyworks

--- src/fsharper/LexerVisualization/MainWindow.xaml.cs
+++ src/fsharper/LexerVisualization/MainWindow.xaml.cs
@@ -98,7 +98,7 @@ namespace JetBrains.ReSharper.FSharp.LexerVisualization
 
       private static void RenderSomethingElse(StringBuilder sb, string tokenText, TokenNodeType token)
       {
-        sb.AppendFormat("<span title=\"maybe operator {1}\">{0}</span>", tokenText, WebUtility.HtmlEncode(tokenText));
+        sb.AppendFormat("<span title=\"maybe operator {0}\">{0}</span>", tokenText);
       }
 
       private static void RenderIdentifier(StringBuilder sb, string tokenText, TokenNodeType token)
