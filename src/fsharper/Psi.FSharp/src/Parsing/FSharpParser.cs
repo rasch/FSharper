269ced9cb9281870c34416fb242f33ca1bd94d41
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2013-05-09 19:00:47 +0400

    fixes #52
    but what did I just fix?

--- src/fsharper/Psi.FSharp/src/Parsing/FSharpParser.cs
+++ src/fsharper/Psi.FSharp/src/Parsing/FSharpParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using JetBrains.Application;
 using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
 using JetBrains.ReSharper.Psi.FSharp.Gen;
@@ -6,6 +7,16 @@ using JetBrains.ReSharper.Psi.FSharp.Impl.Tree;
 using JetBrains.ReSharper.Psi.FSharp.Tree;
 using JetBrains.ReSharper.Psi.Parsing;
 using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.Text;
+using JetBrains.Util;
+using System;
+using JetBrains.Application;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
+using JetBrains.ReSharper.Psi.Parsing;
+using JetBrains.Text;
+using JetBrains.Util;
+using JetBrains.Util.DataStructures;
 
 namespace JetBrains.ReSharper.Psi.FSharp.Parsing
 {  
@@ -124,11 +135,148 @@ namespace JetBrains.ReSharper.Psi.FSharp.Parsing
     }
   }
 
-  internal class FSharpMissingTokensInserter
+  internal class FSharpMissingTokensInserter : MissingTokenInserterBase
   {
-    public static void Run(TreeElement result, ILexer<int> myOriginalLexer, FSharpParser fSharpParser, bool trimMissingTokens, SeldomInterruptChecker myCheckForInterrupt)
+    private readonly ILexer lexer;
+    private new readonly DataIntern<string> whitespaceIntern = new DataIntern<string>();
+
+    private FSharpMissingTokensInserter(ILexer lexer, ITokenOffsetProvider offsetProvider, SeldomInterruptChecker interruptChecker)
+      : base(offsetProvider, interruptChecker)
     {
-      //TODO
+      this.lexer = lexer;
+    }
+
+    public static void Run(TreeElement node, ILexer lexer, ITokenOffsetProvider offsetProvider, bool trimTokens, SeldomInterruptChecker interruptChecker)
+    {
+      Assertion.Assert(node.parent != null, "node.parent!=null");
+      var root = node as CompositeElement;
+      if (root == null) return;
+      var inserter = new FSharpMissingTokensInserter(lexer,
+        offsetProvider, interruptChecker);
+      lexer.Start();
+
+      if (trimTokens)
+      {
+        using (var container = new DummyContainer(root))
+          inserter.Run(container);
+      }
+      else
+      {
+        var terminator = new EofToken(lexer.Buffer);
+        root.AppendNewChild(terminator);
+        inserter.Run(root);
+        root.DeleteChildRange(terminator, terminator);
+      }
+    }
+
+    private sealed class EofToken : BindedToBufferLeafElement
+    {
+      public EofToken(IBuffer buffer)
+        : base(FSharpTokenType.EOF, buffer, new TreeOffset(buffer.Length), new TreeOffset(buffer.Length))
+      {
+      }
+
+      public override PsiLanguageType Language
+      {
+        get { return FSharpLanguage.Instance; }
+      }
+    }
+
+    private sealed class DummyContainer : CompositeElement, IDisposable
+    {
+      public DummyContainer(TreeElement element)
+      {
+        AppendNewChild(element);
+      }
+
+      public override NodeType NodeType
+      {
+        get { return DummyNodeType.Instance; }
+      }
+
+      public override PsiLanguageType Language
+      {
+        get { return FSharpLanguage.Instance; }
+      }
+
+      public void Dispose()
+      {
+        DeleteChildRange(firstChild, firstChild);
+      }
+
+      private sealed class DummyNodeType : CompositeNodeType
+      {
+        public static readonly NodeType Instance = new DummyNodeType();
+
+        private DummyNodeType()
+          : base("DummyContainer")
+        {
+        }
+
+        //public override PsiLanguageType LanguageType { get { return UnknownLanguage.Instance; } }
+        public override CompositeElement Create()
+        {
+          throw new InvalidOperationException();
+        }
+      }
+    }
+
+    protected override void ProcessLeafElement(TreeElement element)
+    {
+      int leafOffset = GetLeafOffset(element).Offset;
+
+      // Check if some tokens are missed before this leaf
+      if (lexer.TokenType != null && lexer.TokenStart < leafOffset)
+      {
+        // Find out the right place to insert tokens to
+        TreeElement anchor = element;
+        CompositeElement parent = anchor.parent;
+        while (anchor == parent.firstChild && parent.parent != null)
+        {
+          anchor = parent;
+          parent = parent.parent;
+        }
+
+        // proceed with inserting tokens
+        while (lexer.TokenType != null && lexer.TokenStart < leafOffset)
+        {
+          LeafElementBase token = CreateMissingToken();
+
+          parent.AddChildBefore(token, anchor);
+
+          lexer.Advance();
+        }
+      }
+
+      // skip all tokens which lie inside given leaf element
+      int leafEndOffset = leafOffset + element.GetTextLength();
+      if ((element is IClosedChameleonBody) && (lexer is CachingLexer))
+      {
+        ((CachingLexer)lexer).FindTokenAt(leafEndOffset);
+      }
+      else
+      {
+        while (lexer.TokenType != null && lexer.TokenStart < leafEndOffset)
+        {
+          lexer.Advance();
+        }
+      }
+    }
+
+    private LeafElementBase CreateMissingToken()
+    {
+      TokenNodeType tokenType = lexer.TokenType;
+      if (tokenType == FSharpTokenType.WHITE_SPACE || tokenType == FSharpTokenType.NEW_LINE)
+      {
+        string text = lexer.GetCurrTokenText();
+        if (tokenType == FSharpTokenType.WHITE_SPACE)
+        {
+          return new Whitespace(whitespaceIntern.Intern(text));
+        }
+        return new NewLine(text);
+      }
+
+      return TreeElementFactory.CreateLeafElement(lexer);
     }
   }
 }
